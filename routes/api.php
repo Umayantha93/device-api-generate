77e5936a5dd5d55d7bd06f57cb193e79023d6787
Umayantha93 <umayantha93@gmail.com>
2025-01-19 19:18:39 +0530

    middleware

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\DeviceController;
 use App\Http\Controllers\LeasingController;
+use App\Http\Middleware\Authenticate;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,4 +12,6 @@ Route::get('/user', function (Request $request) {
 
 
 Route::post('/register', [DeviceController::class, 'register']);
-Route::get('/device/info/{id}', [LeasingController::class, 'getInfo']);
+
+Route::get('/device/info/{id}', [LeasingController::class, 'getInfo'])->middleware(Authenticate::class);
+Route::put('/device/update/{id}', [LeasingController::class, 'updateLeasing']);
